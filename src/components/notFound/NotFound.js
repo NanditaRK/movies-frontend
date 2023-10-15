cee46802617c1ace0cdb1da8b7ddddde4c2b846b
Nandita <95146588+NanditaRK@users.noreply.github.com>
2023-10-14 17:21:06 -0700

    Add files via upload

---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        This page could not be found. Please redirect to the home page.
+    </div>
+  )
+}
+
+export default NotFound;
